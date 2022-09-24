f5cc789bb406bba627d751b2bd2bd2c8f7522f6e
Mariia Sechenova <sechenova.m.a@yandex.ru>
2022-09-24 09:47:48 +0700

    first task

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        int books = 20;
+        byte pages = 50;
+        short totalPages = 1000;
+        long totalWords = 1000001L;
+        float wordsPerPage = 2000.02F;
+        double wordsPerBook = 200000.2;
+        System.out.println("Значение переменной books с типом int равно " + books);
+        System.out.println("Значение переменной pages с типом byte равно " + pages);
+        System.out.println("Значение переменной totalPages с типом short равно " + totalPages);
+        System.out.println("Значение переменной totalWords с типом long равно " + totalWords);
+        System.out.println("Значение переменной wordsPerPage с типом float равно " + wordsPerPage);
+        System.out.println("Значение переменной wordsPerBook с типом double равно " + wordsPerBook);
+    }
+}
